bf9ab56dc5b6cb827d284babab7d29877f115aa7
carlosa-suarez <ingcarlos.suarez.moreno@gmail.com>
2016-08-28 21:50:07 -0500

    compose post function

--- app/compose/compose.js
+++ app/compose/compose.js
@@ -13,9 +13,28 @@ angular.module('inboxApp.compose', ['ngRoute'])
 .controller('newCtrl', newCtrl);
 
 function newCtrl ($http) {
-  var vm = this;
+  var vm = this, url, config;
 
-  vm.newMails = {};
 
-  
+  vm.model = {};
+  vm.submit = function() {
+    if (vm.newMailForm.valid) {
+      url = 'http://private-8c78d-inboxapi5.apiary-mock.com/new'
+      config = {
+          headers : {
+              'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+          }
+      }
+                                              
+      $http.post(url, vm.model, config)
+      .then(
+        function(response){
+           // success callback
+        }, 
+        function(response){
+         // failure callback
+        }
+      );
+    }
+  };
 }
