0e2202018661fc9617b153dd5148f61e96f1d742
carlosa-suarez <ingcarlos.suarez.moreno@gmail.com>
2016-08-29 18:38:33 -0500

    compose post request fixed

--- app/compose/compose.js
+++ app/compose/compose.js
@@ -12,24 +12,25 @@ angular.module('inboxApp.compose', ['ngRoute'])
 
 .controller('newCtrl', newCtrl);
 
-function newCtrl ($http) {
+function newCtrl ($http, $scope) {
   var vm = this, url, config;
-
 
   vm.model = {};
   vm.submit = function() {
-    if (vm.newMailForm.valid) {
-      url = 'http://private-8c78d-inboxapi5.apiary-mock.com/new'
+    
+    if ($scope.newMailForm.$valid) {
+      console.log($scope);
+      url = 'http://private-8c78d-inboxapi5.apiary-mock.com/new';
       config = {
-          headers : {
-              'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
-          }
-      }
+        headers : {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+        }
+      };
                                               
       $http.post(url, vm.model, config)
       .then(
         function(response){
-           // success callback
+           console.log(ff);
         }, 
         function(response){
          // failure callback
